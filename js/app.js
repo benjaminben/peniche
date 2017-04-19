9b6bcdd71fc4133d76ee0fca1502678a62c5f3e2
benBen <benmbenjamin@gmail.com>
2017-04-19 16:45:46 -0700

    fix loader logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,28 @@
 (function() {
-  console.log("hi")
+  var main = document.querySelector("main")
+  var loader = document.getElementById("intro_loader")
+  var loaderCta = document.getElementById("loader_cta")
+  var imgs = document.getElementsByTagName("img")
+  var loaded = 0
+
+  var initEnter = function() {
+    loader.className += " ready"
+    loader_cta.textContent = "ENTER"
+    loader_cta.addEventListener("click", function() {
+      loader.style.display = "none"
+      main.className += " active"
+    })
+  }
+
+  for (var i = 0; i < imgs.length; i++) {
+    var img = new Image()
+    img.onload = function() {
+      loaded++
+      if (loaded === imgs.length) {
+        initEnter()
+      }
+    }
+    img.src = imgs[i].src
+  }
+
 })()
