32a4823ca9233a15715365ad07b1efbd38cd4154
benBen <benmbenjamin@gmail.com>
2017-04-20 08:25:27 -0700

    whole loader clickable

--- js/app.js
+++ js/app.js
@@ -2,13 +2,14 @@
   var main = document.querySelector("main")
   var loader = document.getElementById("intro_loader")
   var loaderCta = document.getElementById("loader_cta")
+  var loaderCont = document.querySelector("#intro_loader .cont")
   var imgs = document.getElementsByTagName("img")
   var loaded = 0
 
   var initEnter = function() {
     loader.className += " ready"
     loader_cta.textContent = "ENTER"
-    loader_cta.addEventListener("click", function() {
+    loaderCont.addEventListener("click", function() {
       loader.style.display = "none"
       main.className += " active"
     })
